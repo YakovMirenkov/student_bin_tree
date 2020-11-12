b8bd21555b93e3093f5f53549081ea075dff390c
Yakov Mirenkov <Yakov.Mirenkov@student.msu.ru>
2020-11-12 08:51:53 +0300

    tree: fix max_diff_depths()

--- student/tree/max_diff_depths.cpp
+++ student/tree/max_diff_depths.cpp
@@ -4,7 +4,7 @@ int diff_depths_node(tree_node *node, int &max_diff);
 
 int tree::max_diff_depths() const
 {
-	int max_diff = 0;
+	int max_diff = -1;
 
 	diff_depths_node(root, max_diff);
 
@@ -13,19 +13,22 @@ int tree::max_diff_depths() const
 
 int diff_depths_node(tree_node *node, int &max_diff)
 {
-	int depth_left, depth_right, diff;
+	int depth_left, depth_right;
 
 	if( node==nullptr )
-		return 0;
-	if( node->get_left()==nullptr && node->get_right()==nullptr )
-		return 1;
+		return -1;
 
 	depth_left = diff_depths_node(node->get_left(), max_diff);
 	depth_right = diff_depths_node(node->get_right(), max_diff);
-	diff = abs(depth_left - depth_right);
 
-	if( diff>max_diff )
-		max_diff = diff;
+	if( depth_left>=0 && depth_right>=0 )
+	{
+		int diff = abs(depth_left - depth_right);
+
+		if( diff>max_diff )
+			max_diff = diff;
+	}
+
 	if( depth_left>depth_right )
 		return 1 + depth_left;
 	return 1 + depth_right;
